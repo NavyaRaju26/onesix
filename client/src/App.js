a5f171edd11c8d5878dc342b7ac3f95b15aa1ad2
NavyaRaju26 <nr0642387@gmail.com>
2026-03-09 06:50:19 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ const [email,setEmail] = useState("");
 
 const addStudent = () => {
 
-axios.post("http://localhost:5000/add",{
+axios.post("https://student-registration-api-8xbm.onrender.com/add",{
 name:name,
 email:email
 }).then(()=>{
@@ -19,82 +19,30 @@ alert("Student Added Successfully");
 
 return(
 
-<div style={styles.container}>
+<div style={{textAlign:"center",marginTop:"50px"}}>
 
-<div style={styles.card}>
-
-<h2 style={styles.title}>Student Registration</h2>
+<h2>Student Registration</h2>
 
 <input
-style={styles.input}
-type="text"
 placeholder="Enter Name"
 onChange={(e)=>setName(e.target.value)}
 />
 
+<br/><br/>
+
 <input
-style={styles.input}
-type="email"
 placeholder="Enter Email"
 onChange={(e)=>setEmail(e.target.value)}
 />
 
-<button style={styles.button} onClick={addStudent}>
-Submit
-</button>
+<br/><br/>
 
-</div>
+<button onClick={addStudent}>Submit</button>
 
 </div>
 
 );
 
 }
-
-const styles = {
-
-container:{
-height:"100vh",
-display:"flex",
-justifyContent:"center",
-alignItems:"center",
-background:"linear-gradient(135deg,#74ebd5,#ACB6E5)",
-fontFamily:"Arial"
-},
-
-card:{
-background:"white",
-padding:"40px",
-borderRadius:"10px",
-boxShadow:"0 8px 20px rgba(0,0,0,0.2)",
-textAlign:"center",
-width:"300px"
-},
-
-title:{
-marginBottom:"20px"
-},
-
-input:{
-width:"100%",
-padding:"10px",
-margin:"10px 0",
-borderRadius:"5px",
-border:"1px solid #ccc",
-fontSize:"14px"
-},
-
-button:{
-width:"100%",
-padding:"10px",
-background:"#4CAF50",
-color:"white",
-border:"none",
-borderRadius:"5px",
-fontSize:"16px",
-cursor:"pointer"
-}
-
-};
 
 export default App;
